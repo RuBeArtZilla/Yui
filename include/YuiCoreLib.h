b0521948cd6758eb13a7ef4df75240d77d507ac9
Artyom Fomin <mail@artzilla.name>
2013-10-03 16:02:56 +0700

    Created new solution (YuiMain)

---
+++ include/YuiCoreLib.h
@@ -0,0 +1,10 @@
+#ifndef YUI_CORE_LIB_H
+#define YUI_CORE_LIB_H
+#pragma once
+
+#include "../YuiCore/YuiCore.h"
+#include "../YuiCore/YuiModuleManager.h"
+
+#define UNUSED_DEFINE 1
+
+#endif /* YUI_CORE_LIB_H */
